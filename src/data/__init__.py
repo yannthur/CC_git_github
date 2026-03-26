0fef55b553146e252de481b28bbe050c5e857717
NZOGNI Yann <nzognioyann@gmail.com>
2026-03-26 17:04:56 +0100

    Fix gitignore, add datasets and Data_download module

---
+++ src/data/__init__.py
@@ -0,0 +1,8 @@
+"""Data loading utilities."""
+
+from torch.utils.data import DataLoader
+
+
+def load_data(data_config: dict) -> tuple[DataLoader, DataLoader]:
+    """Load and return train/val dataloaders."""
+    return None, None  # TODO: implement
